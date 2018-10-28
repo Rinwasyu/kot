7cd0a53423b52551ea2d05f1af665c102b3e5502
Rinwasyu <rinwakun@gmail.com>
2018-10-28 23:11:32 +0900

    fix a method name mistake (fix #9)

--- src/cursor.c
+++ src/cursor.c
@@ -117,7 +117,7 @@ void cursor_down(struct Cursor *cursor) {
 	}
 }
 
-void cursor_home(struct Cursor *cursor) {
+void cursor_end(struct Cursor *cursor) {
 	if (editor.col + ws.ws_col - (int)strlen(doc.buf[editor.row + cursor->row]) >= 0) {
 		cursor->col = (int)strlen(doc.buf[editor.row + cursor->row]) - editor.col;
 	} else {
@@ -126,7 +126,7 @@ void cursor_home(struct Cursor *cursor) {
 	}
 }
 
-void cursor_end(struct Cursor *cursor) {
+void cursor_home(struct Cursor *cursor) {
 	editor.col = 0;
 	cursor->col = 0;
 }
@@ -139,6 +139,6 @@ struct Cursor cursor = {
 	cursor_left,
 	cursor_up,
 	cursor_down,
-	cursor_home,
-	cursor_end
+	cursor_end,
+	cursor_home
 };
